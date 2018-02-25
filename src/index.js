12dada1a520fdb1fea9ffa32536ce145b40f7773
ValysyaPoncho <valentin.ivancikov@gmail.com>
2018-02-25 14:37:27 +0300

    6th solution 0.9

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = 
+function check(str, bracketsConfig) {
+  var stack = [], stroka = [];
+
+  for (var i = 0; i < str.length; i++) {
+    stroka[i] = str[i];
+  }
+
+  if (stroka.length % 2 !== 0)
+    return false;
+
+  for (var i = 0; i < stroka.length; i++) {
+    for (var j = 0; j < bracketsConfig.length; j++) {
+
+      if (stroka[i] === bracketsConfig[j][0] && stack.length === 0) {
+        stack.push(stroka[i]); j = 0;
+        break;  
+      }
+
+      if (stroka[i] === bracketsConfig[j][1] && bracketsConfig[j][1] !== bracketsConfig[j][0] && stack.length === 0)
+        return false;
+        //console.log("fl1");
+
+      if (stack.length !== 0 && stroka[i] === bracketsConfig[j][1] && bracketsConfig[j][1] !== bracketsConfig[j][0]) {
+        var z = stack.pop();
+        if (z !== bracketsConfig[j][0])
+          return false;
+          //console.log("fl1.2");
+      }
+
+      if (stack.length !== 0 && stroka[i] === bracketsConfig[j][0]) {
+        if (bracketsConfig[j][0] === bracketsConfig[j][1] && bracketsConfig[j][0] === stack[stack.length - 1]) {
+          stack.pop(); j = 0;
+          break;
+        } else {
+          if (bracketsConfig[j][0] === bracketsConfig[j][1] && bracketsConfig[j][0] !== stack[stack.length - 1]) {
+            stack.push(stroka[i]); j = 0;
+            break;
+          }
+        } 
+        if (stroka[i] === bracketsConfig[j][0]) {
+          stack.push(stroka[1]); j = 0;
+          break;
+        } else {
+          if (stroka[i] === bracketsConfig[j][1] && bracketsConfig[j][0] !== bracketsConfig[j][1]) {
+            var c = stack.pop(); j = 0;
+            if (c !== bracketsConfig[j][0])
+              return false;
+              //console.log("fl2");
+          }
+        }
+      }
+   }
+  }
+  //console.log("tr");
+  return true;
 }
+/*
+const config1 = [['(', ')']];
+const config2 = [['(', ')'], ['[', ']']];
+const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
+const config4 = [['|', '|']];
+const config5 = [['(', ')'], ['|', '|']];
+const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
+const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
+
+check('([{}])', config3);*/
